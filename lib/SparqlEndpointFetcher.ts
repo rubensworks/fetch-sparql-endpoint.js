a9f45d598f4d92367e1cdce1f209b30b8925de29
Ruben Taelman <ruben.taelman@ugent.be>
2018-08-01 09:55:23 +0200

    Allow a custom data factory to be provided

--- lib/SparqlEndpointFetcher.ts
+++ lib/SparqlEndpointFetcher.ts
@@ -1,13 +1,23 @@
+import * as RDF from "rdf-js";
+
 /**
  * A SparqlEndpointFetcher can send queries to SPARQL endpoints,
  * and retrieve and parse the results.
  */
 export class SparqlEndpointFetcher {
 
-  public readonly fetchCb?: (input?: Request | string, init?: RequestInit) => Promise<Response>;
+  public readonly fetchCb: (input?: Request | string, init?: RequestInit) => Promise<Response>;
+  public readonly dataFactory: RDF.DataFactory;
 
-  constructor(fetchCb?: (input?: Request | string, init?: RequestInit) => Promise<Response>) {
-    this.fetchCb = fetchCb || require('isomorphic-fetch');
+  constructor(args?: ISparqlEndpointFetcherArgs) {
+    args = args || {};
+    this.fetchCb = args.fetch || require('isomorphic-fetch');
+    this.dataFactory = args.dataFactory || require('rdf-data-model');
   }
 
+}
+
+export interface ISparqlEndpointFetcherArgs {
+  fetch?: (input?: Request | string, init?: RequestInit) => Promise<Response>;
+  dataFactory?: RDF.DataFactory;
 }
--- test/SparqlEndpointFetcher-test.ts
+++ test/SparqlEndpointFetcher-test.ts
@@ -2,9 +2,23 @@ import {SparqlEndpointFetcher} from "../lib/SparqlEndpointFetcher";
 
 describe('SparqlEndpointFetcher', () => {
 
-  describe('constructed without fetch callback', () => {
+  describe('constructed without fetch callback and data factory', () => {
     it('should have the default fetch function', async () => {
       return expect(new SparqlEndpointFetcher().fetchCb).toBe(require('isomorphic-fetch'));
+    });
+
+    it('should have the default data factory', async () => {
+      return expect(new SparqlEndpointFetcher().dataFactory).toBe(require('rdf-data-model'));
+    });
+  });
+
+  describe('constructed without fetch callback and with data factory', () => {
+    it('should have the default fetch function', async () => {
+      return expect(new SparqlEndpointFetcher().fetchCb).toBe(require('isomorphic-fetch'));
+    });
+
+    it('should have the given data factory', async () => {
+      return expect(new SparqlEndpointFetcher({ dataFactory: <any> 'abc' }).dataFactory).toEqual('abc');
     });
   });
 
@@ -15,7 +29,7 @@ describe('SparqlEndpointFetcher', () => {
 
     beforeEach(() => {
       fetchCb = (url) => Promise.resolve(new Response('dummy'));
-      fetcher = new SparqlEndpointFetcher(fetchCb);
+      fetcher = new SparqlEndpointFetcher({ fetch: fetchCb });
     });
 
     it('should have the default fetch function', async () => {
