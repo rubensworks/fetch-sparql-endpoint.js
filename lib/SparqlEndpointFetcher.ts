e26b8b9abb14ee6009753f02395529ff62b66871
Ruben Taelman <ruben.taelman@ugent.be>
2018-08-01 10:05:55 +0200

    Add parseJsonBindings method

--- lib/SparqlEndpointFetcher.ts
+++ lib/SparqlEndpointFetcher.ts
@@ -15,9 +15,43 @@ export class SparqlEndpointFetcher {
     this.dataFactory = args.dataFactory || require('rdf-data-model');
   }
 
+  /**
+   * Convert a SPARQL JSON result binding to a bindings object.
+   * @param rawBindings A SPARQL json result binding.
+   * @return {Bindings} A bindings object.
+   */
+  public parseJsonBindings(rawBindings: any): Bindings {
+    const bindings: Bindings = {};
+    for (const key in rawBindings) {
+      const rawValue: any = rawBindings[key];
+      let value: RDF.Term = null;
+      switch (rawValue.type) {
+      case 'bnode':
+        value = this.dataFactory.blankNode(rawValue.value);
+        break;
+      case 'literal':
+        if (rawValue['xml:lang']) {
+          value = this.dataFactory.literal(rawValue.value, rawValue['xml:lang']);
+        } else if (rawValue.datatype) {
+          value = this.dataFactory.literal(rawValue.value, this.dataFactory.namedNode(rawValue.datatype));
+        } else {
+          value = this.dataFactory.literal(rawValue.value);
+        }
+        break;
+      default:
+        value = this.dataFactory.namedNode(rawValue.value);
+        break;
+      }
+      bindings['?' + key] = value;
+    }
+    return bindings;
+  }
+
 }
 
 export interface ISparqlEndpointFetcherArgs {
   fetch?: (input?: Request | string, init?: RequestInit) => Promise<Response>;
   dataFactory?: RDF.DataFactory;
 }
+
+export type Bindings = {[key: string]: RDF.Term};
--- test/SparqlEndpointFetcher-test.ts
+++ test/SparqlEndpointFetcher-test.ts
@@ -1,3 +1,4 @@
+import {blankNode, literal, namedNode} from "rdf-data-model";
 import {SparqlEndpointFetcher} from "../lib/SparqlEndpointFetcher";
 
 describe('SparqlEndpointFetcher', () => {
@@ -34,6 +35,54 @@ describe('SparqlEndpointFetcher', () => {
 
     it('should have the default fetch function', async () => {
       return expect(fetcher.fetchCb).toBe(fetchCb);
+    });
+
+    describe('#parseJsonBindings', () => {
+      it('should convert bindings with named nodes', () => {
+        return expect(fetcher.parseJsonBindings({
+          book: { type: 'uri', value: 'http://example.org/book/book6' },
+        })).toEqual({ '?book': namedNode('http://example.org/book/book6') });
+      });
+
+      it('should convert bindings with blank nodes', () => {
+        return expect(fetcher.parseJsonBindings({
+          book: { type: 'bnode', value: 'abc' },
+        })).toEqual({ '?book': blankNode('abc') });
+      });
+
+      it('should convert bindings with literals', () => {
+        return expect(fetcher.parseJsonBindings({
+          book: { type: 'literal', value: 'abc' },
+        })).toEqual({ '?book': literal('abc') });
+      });
+
+      it('should convert bindings with languaged literals', () => {
+        return expect(fetcher.parseJsonBindings({
+          book: { 'type': 'literal', 'value': 'abc', 'xml:lang': 'en-us' },
+        })).toEqual({ '?book': literal('abc', 'en-us') });
+      });
+
+      it('should convert bindings with datatyped literals', () => {
+        return expect(fetcher.parseJsonBindings({
+          book: { type: 'literal', value: 'abc', datatype: 'http://ex' },
+        })).toEqual({ '?book': literal('abc', namedNode('http://ex')) });
+      });
+
+      it('should convert mixed bindings', () => {
+        return expect(fetcher.parseJsonBindings({
+          book1: { type: 'uri', value: 'http://example.org/book/book6' },
+          book2: { type: 'bnode', value: 'abc' },
+          book3: { type: 'literal', value: 'abc' },
+          book4: { 'type': 'literal', 'value': 'abc', 'xml:lang': 'en-us' },
+          book5: { type: 'literal', value: 'abc', datatype: 'http://ex' },
+        })).toEqual({
+          '?book1': namedNode('http://example.org/book/book6'),
+          '?book2': blankNode('abc'),
+          '?book3': literal('abc'),
+          '?book4': literal('abc', 'en-us'),
+          '?book5': literal('abc', namedNode('http://ex')),
+        });
+      });
     });
   });
 
