f3256d250454ff8806b59c6f9c09ef8a741074a6
Ruben Taelman <ruben.taelman@ugent.be>
2018-08-01 13:26:01 +0200

    Add fetch-sparql-endpoint bin

---
+++ bin/fetch-sparql-endpoint.ts
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+import {readFileSync} from "fs";
+import minimist = require("minimist");
+import {quadToStringQuad, termToString} from "rdf-string";
+import {SparqlEndpointFetcher} from "../index";
+
+process.argv.splice(0, 2);
+const args = minimist(process.argv);
+if (args._.length === 0 || args._.length > 2 || args.h || args.help) {
+
+  // Print command usage
+  process.stderr.write(`fetch-sparql-endpoint Sends a query to a SPARQL endpoint
+
+Usage:
+  fetch-sparql-endpoint https://dbpedia.org/sparql [-q] 'SELECT * WHERE { ?s ?p ?o }'
+  fetch-sparql-endpoint https://dbpedia.org/sparql -f query.sparql
+  cat query.sparql | fetch-sparql-endpoint https://dbpedia.org/sparql
+
+Options:
+  -q            evaluate the given SPARQL query string
+  -f            evaluate the SPARQL query in the given file
+  --help        print this help message
+`);
+  process.exit(1);
+}
+
+async function getQuery(): Promise<string> {
+  if (args._.length > 1) {
+    return args._[1];
+  } else if (args.q) {
+    return args.q;
+  } else if (args.f) {
+    return readFileSync(args.f, { encoding: 'utf8' });
+  } else {
+    // tslint:disable-next-line:no-var-requires
+    return await require('stream-to-string')(process.stdin);
+  }
+}
+
+const endpoint = args._[0];
+
+getQuery().then((query) => {
+  const fetcher = new SparqlEndpointFetcher();
+  const queryType = fetcher.getQueryType(query);
+  switch (queryType) {
+  case 'SELECT':
+    querySelect(fetcher, query);
+    break;
+  case 'ASK':
+    queryAsk(fetcher, query);
+    break;
+  case 'CONSTRUCT':
+    queryConstruct(fetcher, query);
+    break;
+  }
+});
+
+function querySelect(fetcher: SparqlEndpointFetcher, query: string) {
+  fetcher.fetchBindings(endpoint, query)
+    .then((bindingsStream) => {
+      bindingsStream.on('data', (bindings) => {
+        for (const variable of Object.keys(bindings)) {
+          bindings[variable] = termToString(bindings[variable]);
+        }
+        process.stdout.write(JSON.stringify(bindings) + '\n');
+      });
+    })
+    .catch((error) => {
+      process.stderr.write(error.message + '\n');
+      process.exit(1);
+    });
+}
+
+function queryAsk(fetcher: SparqlEndpointFetcher, query: string) {
+  fetcher.fetchAsk(endpoint, query)
+    .then((answer) => {
+      process.stdout.write(answer + '\n');
+    })
+    .catch((error) => {
+      process.stderr.write(error.message + '\n');
+      process.exit(1);
+    });
+}
+
+function queryConstruct(fetcher: SparqlEndpointFetcher, query: string) {
+  fetcher.fetchTriples(endpoint, query)
+    .then((tripleStream) => {
+      tripleStream.on('data', (triple) => {
+        const tripleString = quadToStringQuad(triple);
+        process.stdout.write(JSON.stringify(tripleString, null, '  ') + '\n');
+      });
+    })
+    .catch((error) => {
+      process.stderr.write(error.message + '\n');
+      process.exit(1);
+    });
+}
