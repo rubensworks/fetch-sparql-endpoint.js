68307da7e8ee6e9111a013636dcf4f00b406a1a3
Ruben Taelman <ruben.taelman@ugent.be>
2023-09-11 16:03:10 +0200

    Fix Docker tests failing due to Docker image breakage

--- .github/workflows/start-docker.sh
+++ .github/workflows/start-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker pull stain/jena-fuseki
+docker pull stain/jena-fuseki@sha256:4d84eb09dc69603cab990a25a5ad683cec648159f39ccf5dbffba312e7d7666a
 docker container create --name endpoint \
   -p 4000:3030 \
   -e FUSEKI_DATASET_1=mydataset \
   -v $(pwd)/.github/workflows/shiro.ini:/jena-fuseki/shiro.ini \
-  stain/jena-fuseki
+  stain/jena-fuseki@sha256:4d84eb09dc69603cab990a25a5ad683cec648159f39ccf5dbffba312e7d7666a
 docker start endpoint
